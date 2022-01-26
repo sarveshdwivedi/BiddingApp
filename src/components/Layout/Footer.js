6c819938200c36a422f1c63f0dd8ec7cfe5f805a
Sarvesh Dwivedi <sarvesh001@gmail.com>
2022-01-26 20:37:06 +0530

    Add files via upload

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Footer.css';
+
+export default function Footer(props) {
+    return (
+        <footer role="contentinfo">
+            <p></p>
+        </footer>
+    )
+}
